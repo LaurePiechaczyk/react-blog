232027b6d9c3dbadda1c30a29e58526ce5ece0c5
LaurePiechaczyk <laure.piecha@gmail.com>
2022-03-27 17:37:40 +0000

    home page with ome card

---
+++ blog-react/src/Containers/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Home.css'
+import Card from "../../Components/Card/Card";
+
+export default function Home() {
+  return (
+    <>
+        <h1 className='home-title'>All Posts</h1>
+        <div className='container-cards'>
+            <Card>
+                <h2>dddd</h2>
+            </Card>
+        </div>
+    </>
+  )
+}
